82df319352ead431a8a5c190b447000b1926f517
Scuftz <shivneel.singh1600@gmail.com>
2021-05-05 17:08:16 +1200

    Add yaml file and other untracked files

---
+++ models/Article.js
@@ -0,0 +1,37 @@
+// models/Article.js
+
+const mongoose = require('mongoose');
+
+const ArticleSchema = new mongoose.Schema({
+  article_id: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  year_of_pub: {
+    type: Number
+  },
+  journal_name: {
+    type: String,
+    required: true
+  },
+  volume_number: {
+    type: Number,
+    required: true
+  },
+  doi: {
+    type: String,
+  },
+  process_status: {
+    type: String,
+  }
+});
+
+module.exports = Article = mongoose.model('article', ArticleSchema);
