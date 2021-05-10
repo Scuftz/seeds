92f1468ca8398393700c2d6c764eb7ea5cedabf7
Scuftz <shivneel.singh1600@gmail.com>
2021-05-10 22:31:11 +1200

    Fixed test to pass

--- seeds-app/src/App.test.js
+++ seeds-app/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Articles List/i);
+  const linkElement = screen.getByText(/Moderator/i);
   expect(linkElement).toBeInTheDocument();
 });
